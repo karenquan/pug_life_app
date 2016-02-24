817f778531f3e07af11ef390cc0326a2378d13c5
Karen Quan <quan.karen@gmail.com>
2016-02-23 16:06:10 -0800

    add dynamic modal to root view

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -10,15 +10,31 @@ var Images = (function(){
       });
 
 
-      $('.image').on('click', function(e) {
-        $(this).find('.modal').removeClass('hide');
-        $(this).find('.close').on('click', function() {
-          console.log($(this).closest('.modal'));
-          $($(this).closest('.modal')).addClass('hide');
-        });
-      });
-  }
+      $('.image').on('click', imageClickEvent);
 
+      function imageClickEvent() {
+        //build dynamic image modal
+        $('.image').unbind('click');
+
+        $modalContainer = $('<div />', { 'class': 'modal' });
+        $modalContent = $('<div />', { 'class': 'content' });
+          $closeButton = $('<div />', { 'class': 'close' });
+          $imageTitle = $('<h3 />', { text: $(this).find('h3').html() });
+          $imageDescription = $('<p />', { text: $(this).find('.image-description').html() });
+          $imageDate = $('<p />', { text: $(this).find('.image-date').html() });
+          $image = $('<img />', { src: $(this).find('.image-url').html(), alt: $(this).find('h3').html() });
+          $userPath = $('<p />', { html: $(this).find('.image-user-path').html() });
+          $modalContent.append($closeButton).append($imageTitle).append($userPath).append($image).append($imageDescription);
+        $modalContainer.append($modalContent);
+        $('body').append($modalContainer);
+
+        $('.close').on('click', function() {
+          console.log($($(this).closest('.modal')));
+          $('.modal').remove();
+          $('.image').bind('click', imageClickEvent);
+        });
+      }
+  }
 
   function _init() {
     _eventHandlers();
