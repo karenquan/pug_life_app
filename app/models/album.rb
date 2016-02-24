96273f24513c53b6019564a971fe1e49fe948c31
Karen Quan <quan.karen@gmail.com>
2016-02-24 12:59:05 -0800

    add dependent:destroy to album model

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,6 +1,6 @@
 class Album < ActiveRecord::Base
   belongs_to :user
-  has_many :images
+  has_many :images, dependent: :destroy
 
   validates :title, presence: true
   validates :description, presence: true
