05b65be2739c1ccb6d228616e2aeefa3ebaec0fe
Karen Quan <quan.karen@gmail.com>
2016-02-25 09:41:45 -0800

    update controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,20 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  helper_method :current_user
+
+
   private
 
-    helper_method :current_user
-
     def current_user
-      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
 
     def is_admin(user)
       user.is_admin
     end
+
+    # def is_admin?
+    #   current_user.present? && current_user.is_admin
+    # end
 end
