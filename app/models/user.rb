24176e2407aa476b22652bb002738900529c08b9
Karen Quan <quan.karen@gmail.com>
2016-02-25 10:38:44 -0800

    add action and view to delete a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ActiveRecord::Base
   has_attached_file :profile_image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :profile_image, content_type: /\Aimage\/.*\Z/
 
-  has_many :albums
+  has_many :albums, dependent: :destroy
   has_and_belongs_to_many :favorites, class_name: "Image"
 end
